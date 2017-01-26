d9053a38679681b148dbae4e61b9433cf404a328
skylarkJ <janaryndin@gmail.com>
2017-01-25 22:07:36 -0500

    Change style of forms and center game board

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const config = {
-  apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com',
-  },
-};
+    apiOrigins: {
+      production: 'https://aqueous-atoll-85096.herokuapp.com',
+      development: 'http://tic-tac-toe.wdibos.com',
+    },
+  };
 
 module.exports = config;
