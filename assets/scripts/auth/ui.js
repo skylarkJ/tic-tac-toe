254a225ef8893fb1a739016f113bc5267f69cdb6
skylarkJ <janaryndin@gmail.com>
2017-02-05 14:33:15 -0500

    Change Sunday

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,20 +1,48 @@
 'use strict';
 
 const success = (data) => {
+  if (data) {
   // handle success
   console.log(data);
+  $('.message').text('Success!');
+  }
 };
 
 const failure = (error) => {
   // handle failure
   console.error(error);
+  $('.message').text('Error!');
 };
 
-$('.sign-up-form').on('submit', function(){
- $('.sign-up').hide();
-});
+
+const signUpSuccess = function () {
+  $('.message').text('Thank you for signing up!');
+  $('#modalSignUp').removeClass('.modal');
+};
+
+// const signInSuccess = function () {
+//   $('.message').text('You are Successfully signed in!');
+//   $('#modalSignIn').removeClass('.modal');
+// };
+
+
+// const changePasswordSuccess = function () {
+//   $('.message').text('Your password changed!');
+//   $('#modalChangePassword').removeClass('.modal');
+// };
+
+// const signOutSuccess = function () {
+//   $('#button-play').hide();
+//   $('.game-board').hide();
+//   $('.message').text('You have signed out!');
+//   $('#modalSignOut').removeClass('modal');
+// };
 
 module.exports = {
   failure,
   success,
+  signUpSuccess
+  // signInSuccess,
+  // changePasswordSuccess
+  // signOutSuccess,
 };
