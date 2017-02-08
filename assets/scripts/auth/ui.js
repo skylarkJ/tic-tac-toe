0167b325cccae6595b4ba6cb00ffdcbcff8e2dca
skylarkJ <janaryndin@gmail.com>
2017-02-08 16:07:16 -0500

    Change a place of div message and success error

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,6 +4,7 @@ const success = (data) => {
   if (data) {
   // handle success
   console.log(data);
+  $('.message').show();
   $('.message').text('Success!');
   }
 };
@@ -11,23 +12,27 @@ const success = (data) => {
 const failure = (error) => {
   // handle failure
   console.error(error);
+  $('.message').show();
   $('.message').text('Error!');
 };
 
 
 const signUpSuccess = function () {
+  $('.message').show();
   $('.message').text('Thank you for signing up! Now sign in!').css('color', 'blue');
   $('#modalSignUp').modal('hide');
   $('.message').hide(10000);
 };
 
 const signUpError = function () {
+  $('.message').show();
   $('.message').text('Error signing up! Try again.').css('color', 'orange');
   $('#modalSignUp').modal('hide');
 };
 
 const signInSuccess = function () {
-  $('.message').text('You are Successfully signed in!').css('color', 'blue');
+  $('.message').show();
+  $('.message').text('You are successfully signed in!').css('color', 'blue');
   $('.message').hide(10000);
   $('#modalSignIn').modal('hide');
   $('#button-signin-nav').hide();
@@ -39,17 +44,20 @@ const signInSuccess = function () {
 };
 
 const signInError = function () {
+  $('.message').show();
   $('.message').text('Something went wrong. Sign in again!').css('color', 'orange');
   $('#modalSignIn').modal('hide');
 };
 
 const changePasswordSuccess = function () {
+  $('.message').show();
   $('.message').text('Your password has changed!').css('color', 'blue');
   $('#modalChangePassword').modal('hide');
   $('.message').hide(10000);
 };
 
 const changePasswordError = function () {
+  $('.message').show();
   $('.message').text('Changing password failed!').css('color', 'orange');
   $('#modalChangePassword').modal('hide');
 };
@@ -57,18 +65,22 @@ const changePasswordError = function () {
 const signOutSuccess = function () {
   $('#button-play').hide();
   $('.game-board').hide();
-  $('.message').text('You have signed out!');
+  $('.message').show();
+  $('.message').text('You have successfully signed out!');
   $('#modalSignOut').modal('hide').css('color', 'blue');
+  $('.message').hide(10000);
   $('.create-button').hide();
   $('.style-button-password').hide();
   $('#button-signin-nav').show();
+  $('#button-signup-nav').show();
   $('#button-signout-nav').hide();
 };
 
 const signOutError = function () {
   $('#button-play').hide();
   $('.game-board').hide();
-  $('.message').text('You have signed out!');
+  $('.message').show();
+  $('.message').text('Something went wrong! Try to sign out again.');
   $('#modalSignOut').modal('hide').css('color', 'orange');
 };
 
